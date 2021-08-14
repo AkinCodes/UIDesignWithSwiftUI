26333eaeeb3dfa9504f4b77e89eba89cbb559601
akinolussanya <66926466+akinolussanya@users.noreply.github.com>
2021-08-14 14:16:12 +0100

    Initial Commit

---
+++ UIDesignWithSwiftUI/UIDesignWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDesignWithSwiftUIApp.swift
+//  UIDesignWithSwiftUI
+//
+//  Created by Akin on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct UIDesignWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
