cac26d2e3d84c698aeea2ef96207409ae91c0cd5
akinolussanya <66926466+akinolussanya@users.noreply.github.com>
2021-08-14 14:43:52 +0100

    Created this UI project using techniques such as VStack, ZStack, HStack as well as images imported into the assets catalog. I employed image resizing and frames to get the right type of image i needed for this to all come together. What you see is a visually stunning display of a sunset screen with mobile icons on the screen.

--- UIDesignWithSwiftUI/ContentView.swift
+++ UIDesignWithSwiftUI/ContentView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+        ZStack {
+            Image("grad12")
+               .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("rec")
+                    .resizable().opacity(0.0)
+                HStack {
+                    Image("1logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                    Image("2logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                    Image("3logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                }
+                
+                HStack {
+                
+                    Image("4logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                    Image("7logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                    Image("6logo")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .padding()
+                    
+                    
+                }
+            
+            }
+            
+        }
     }
 }
 
